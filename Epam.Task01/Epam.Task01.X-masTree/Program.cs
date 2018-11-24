df008a34d875f854e1ac81992bb1593c82dd98bb
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-11-24 17:05:05 +0400

    Task 01

---
+++ Epam.Task01/Epam.Task01.X-masTree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task01.X_masTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_number;
+            int number_of_lines;
+
+            Console.WriteLine("Please input number of output lines");
+            input_number = Console.ReadLine();
+
+            if ((!(int.TryParse(input_number, out number_of_lines)) || number_of_lines <= 0))
+            {
+                Console.WriteLine("Incorrect input, please input positive integer number");
+            }
+            else
+            {
+                XmasTree(number_of_lines);
+            }
+        }
+        public static void XmasTree(int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    for (int k = 0; k < (2 * N - 1); k++)
+                    {
+                        if ((k >= (N - 1 - j)) && (k <= (N - 1 + j)))
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
