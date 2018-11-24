cb6fe908b7933bbd746674c3eb6a489d43fa79a8
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-11-24 12:56:41 +0400

    no message

---
+++ Epam.Task01/Epam.Task01.Rectangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task01.Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_input, second_input;
+            float A_side, B_side;
+            
+            Console.WriteLine("Please input first side of rectangle");
+            first_input = Console.ReadLine();
+            Console.WriteLine("Please input second side of rectangle");
+            second_input = Console.ReadLine();
+
+            if ((!(float.TryParse(first_input, out A_side)) || A_side <= 0) || (!(float.TryParse(second_input, out B_side)) || B_side <= 0))
+            {
+                Console.WriteLine("Incorrect input, please input positive numbers");
+            }
+            else
+            {
+                Console.WriteLine("Rectangle area is {0}",Rectangle_Area(A_side, B_side));
+            }
+        }
+        static float Rectangle_Area(float a,float b)
+        {
+            return a*b;
+        }
+    }
+}
---
+++ Epam.Task01/Epam.Task01.Triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task01.Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_number;
+            int number_of_lines;
+
+            Console.WriteLine("Please input number of output lines");
+            input_number = Console.ReadLine();
+
+            if ((!(int.TryParse(input_number, out number_of_lines)) || number_of_lines <= 0))
+            {
+                Console.WriteLine("Incorrect input, please input positive integer number");
+            }
+            else
+            {
+                for (int i = 0; i < number_of_lines; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
