79afe58c629a390c3cf3acbba0dff390c0011398
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-11-25 01:01:21 +0400

    Task 01

---
+++ Epam.Task01/Epam.Task01.SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task01.SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int thousand = 1000;
+            int Sum_3_5 = 0;
+            for (int i = 3; i < thousand; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    Sum_3_5 += i;
+                }
+            }
+            Console.WriteLine("Sum of numbers is {0}", Sum_3_5);
+        }
+    }
+}
