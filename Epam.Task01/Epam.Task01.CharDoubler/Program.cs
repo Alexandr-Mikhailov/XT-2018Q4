066d3890675e34be0c5c639f0ea0558a77e2bf05
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-11-25 22:23:28 +0400

    Task 01

---
+++ Epam.Task01/Epam.Task01.CharDoubler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Epam.Task01.CharDoubler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputstring;
+            string stringdoubler;
+            
+            Console.Write("Please input first string to analyze: ");
+            inputstring = Console.ReadLine();
+            
+            Console.Write("Please input second string to analyze: ");
+            stringdoubler = Console.ReadLine();
+            
+            Console.WriteLine("Final string is: {0}", StringCharDoubler(inputstring, stringdoubler));
+        }
+        public static string StringCharDoubler(string inputstr, string doublestr)
+        {
+            StringBuilder temp = new StringBuilder("");
+            for (int i = 0; i < inputstr.Length; i++)
+            {
+                temp.Append(inputstr[i]);
+                for (int j = 0; j < doublestr.Length; j++)
+                {
+                    if (inputstr[i] == doublestr[j])
+                    {
+                        temp.Append(inputstr[i]);
+                        break;
+                    }
+                }
+            }
+            return temp.ToString();
+        }
+    }
+}
