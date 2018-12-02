5e99718ddc38c81d2be990acf6117717361329c2
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-12-02 20:10:05 +0400

    Task 02 Round

---
+++ Epam.Task02/Epam.Task02.Round/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.Round
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x, y, r;
+            string input;
+
+            Console.WriteLine("Input x coordinate of the round");
+            input = Console.ReadLine();
+
+            if (!(double.TryParse(input, out x)))
+            {
+                Console.WriteLine("Coordinate x mast be a number");
+            }
+            else
+            {
+                Console.WriteLine("Input y coordinate of the round");
+                input = Console.ReadLine();
+
+                if (!(double.TryParse(input, out y)))
+                {
+                    Console.WriteLine("Coordinate y mast be a number");
+                }
+                else
+                {
+                    Console.WriteLine("Input radius of the round");
+                    input = Console.ReadLine();
+
+                    if (!(double.TryParse(input, out r)))
+                    {
+                        Console.WriteLine("Radus mast be a number");
+                    }
+                    else
+                    {
+                        Round round = new Round();
+
+                        try
+                        {
+                            round.NewRound(x, y, r);
+                            Console.WriteLine(round.GetCircleLength());
+                            Console.WriteLine(round.GetArea());
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Epam.Task02/Epam.Task02.Round/Round.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.Round
+{
+    public class Round
+    {
+        private double Center_x { get; set; }
+
+        private double Center_y { get; set; }
+
+        private double Radius { get; set; }
+
+        public void NewRound(double x, double y, double radius)
+        {
+            this.Center_x = x;
+            this.Center_y = y;
+
+            if (radius >= 0)
+            {
+                this.Radius = radius;
+            }
+            else
+            {
+                throw new Exception("Radius can not be negative");
+            }
+        }
+
+        public double GetCircleLength()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+    }
+}
