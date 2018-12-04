163070e44d7ad3a5fd17cf3d68bceb9a4bf94d33
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-12-04 22:26:02 +0400

    Task 02

---
+++ Epam.Task02/Epam.Task02.User_/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.User_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name, surename, patronym;
+            string namestring = "Name";
+            string surenamestring = "Surename";
+            string patronymstring = "Patronym";
+            string yearstring = "Year";
+            string monthstring = "Month";
+            string daystring = "Day";
+
+            int year, month, day;
+
+            name = Input(namestring);
+            surename = Input(surenamestring);
+            patronym = Input(patronymstring);
+
+            do
+            {
+                if (int.TryParse(Input(yearstring), out year))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input of year");
+                }
+            }
+            while (true);
+
+            do
+            {
+                if (int.TryParse(Input(monthstring), out month))
+                {
+                    if ((month > 0) && (month < 13))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect number of month");
+                    }
+                }
+            }
+            while (true);
+
+            do
+            {
+                if (int.TryParse(Input(daystring), out day))
+                {
+                    if ((day > 0) && (day <= DateTime.DaysInMonth(year, month)))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect day of {month} month");
+                    }
+                }
+            }
+            while (true);
+
+            DateTime birthdate = new DateTime(year, month, day);
+
+            try
+            {
+                User person = new User(surename, name, patronym, birthdate);
+
+                Console.WriteLine("Surename is {0}", person.Surename);
+                Console.WriteLine("Name is {0}", person.Name);
+                Console.WriteLine("Patronym is {0}", person.Patronym);
+                Console.WriteLine("Birthdate is {0}", person.Birthdate.ToShortDateString());
+                Console.WriteLine("Age is {0}", person.Age);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static string Input(string text)
+        {
+            string tempstring;
+
+            do
+            {
+                Console.WriteLine($"Input {text}");
+                tempstring = Console.ReadLine();
+                tempstring = tempstring.Trim();
+
+                if (!(tempstring.Length == 0))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"{text} can not be blank");
+                }
+            }
+            while (true);
+
+            return tempstring;
+        }
+    }
+}
---
+++ Epam.Task02/Epam.Task02.User_/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.User_
+{
+    public class User
+    {
+        public User(string surename, string name, string patronym, DateTime birthdate)
+        {
+            if (DateTime.Compare(birthdate, DateTime.Now) > 0)
+            {
+                throw new Exception("Date of birth is not correct");
+            }
+            else
+            {
+                this.Surename = surename;
+                this.Name = name;
+                this.Patronym = patronym;
+                this.Birthdate = birthdate;
+                this.Age = DateTime.Now.Year - this.Birthdate.Year;
+            }
+        }
+
+        public string Surename { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronym { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public int Age { get; set; }
+    }
+}
