3a9d6830674129f628740edf8892990c33f159af
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-12-05 18:14:28 +0400

    Task 02

---
+++ Epam.Task02/Epam.Task02.Ring/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.Ring
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double r1, r2;
+            string input;
+
+            do
+            {
+                Console.WriteLine("Input outer radius of the ring");
+                input = Console.ReadLine();
+                if (!double.TryParse(input, out r1))
+                {
+                    Console.WriteLine("Outer radius must be a number");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+
+            do
+            {
+                Console.WriteLine("Input inner radius of the ring");
+                input = Console.ReadLine();
+                if (!double.TryParse(input, out r2))
+                {
+                    Console.WriteLine("Inner radius must be a number");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            while (true);
+
+            try
+            {
+                Ring ring = new Ring(r1, r2);
+                Console.WriteLine("The ring with outer radis {0} and inner radius {1} has area {2}", ring.GetOuterRadius(), ring.GetInnerRadius(), ring.GetArea());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Epam.Task02/Epam.Task02.Ring/Ring.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task02.Ring
+{
+    public class Ring
+    {
+        public Ring(double outer, double inner)
+        {
+            if ((inner < 0) || (outer < 0))
+            {
+                throw new Exception("Radius cannot be less then zero");
+            }
+            else
+            {
+                if (inner > outer)
+                {
+                    throw new Exception("Outer radius cannot be less then inner");
+                }
+                else
+                {
+                    this.Inner = inner;
+                    this.Outer = outer;
+                }
+            }
+        }
+
+        private double Inner { get; set; }
+
+        private double Outer { get; set; }
+
+        public double GetArea()
+        {
+            double area = Math.PI * ((this.Outer * this.Outer) - (this.Inner * this.Inner));
+            return area;
+        }
+
+        public double GetInnerRadius()
+        {
+            return this.Inner;
+        }
+
+        public double GetOuterRadius()
+        {
+            return this.Outer;
+        }
+    }
+}
