754a6110b6f5c4383661045f3a6480e4c94150b6
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-12-10 00:47:00 +0400

    Task 03

---
+++ Epam.Task03/Epam.Task03.Lost/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task03.Lost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int N = 10;
+
+            ArrayList human = new ArrayList();
+
+            for (int i = 1; i <= N; i++)
+            {
+                human.Add($"human {i}");
+            }
+
+            Display(human);
+
+            RemoveEverySecond(human);
+
+            Console.Write("Last person in list is ");
+            Display(human);
+        }
+
+        public static void RemoveEverySecond(ArrayList data)
+        {
+            int count = 1;
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (count % 2 == 0)
+                {
+                    data.RemoveAt(i);
+                    i--;
+                }
+
+                count++;
+                if (i == data.Count - 1)
+                {
+                    i = -1;
+                }
+
+                if (data.Count == 1)
+                {
+                    break;
+                }
+            }
+        }
+
+        public static void Display(ArrayList data)
+        {
+            foreach (var i in data)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Epam.Task03/Epam.Task03.Word frequency/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task03.Word_frequency
+{
+    public class Program
+    {
+        public const char Space = ' ';
+        public const char Dot = '.';
+
+        public static void Main(string[] args)
+        {
+            string input, input_text;
+
+            Console.WriteLine("Input english text");
+            input = Console.ReadLine();
+            input_text = RemovePunctuation(input);
+
+            string[] words = input_text.Split(new char[] { Space, Dot }, StringSplitOptions.RemoveEmptyEntries);
+
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var text in words)
+            {
+                if (dictionary.ContainsKey(text.ToLower()))
+                {
+                    dictionary[text.ToLower()] += 1;
+                }
+                else
+                {
+                    dictionary.Add(text.ToLower(), 1);
+                }
+            }
+
+            Console.WriteLine("Text contains words :");
+
+            foreach (var i in dictionary)
+            {
+                Console.WriteLine($"word {i.Key} {i.Value} time(s)");
+            }
+        }
+
+        public static string RemovePunctuation(string text)
+        {
+            StringBuilder temp = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(text[i]) && ((text[i] != Space) || (text[i] != Dot)))
+                {
+                    temp.Append(Space);
+                }
+                else
+                {
+                    temp.Append(text[i]);
+                }
+            }
+
+            return temp.ToString();
+        }
+    }
+}
