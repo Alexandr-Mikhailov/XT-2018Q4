25826e3ac575e97a59e1efefacdeab190435b960
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2018-12-17 08:18:25 +0400

    Task 04 Custom Sort

---
+++ Epam.Task04/Epam.Task04.CustomSort/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task04.CustomSort
+{
+    public class Program
+    {
+        public delegate int CompareType<T>(T first, T second);
+
+        public static void Sorting<T>(T[] arr, CompareType<T> compare)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (compare(arr[j], arr[i]) < 0)
+                    {
+                        var temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] int_array = new int[]
+            {
+                1, -20, 3, 0, -5, 7, 2, -11, 0, 15
+            };
+
+            double[] double_array = new double[]
+            {
+                1.5, -20.0, 3.7, 0.5, -5.1, 7.9, 2.0, -11.24, 0.0, 15.8
+            };
+
+            char[] char_array = new char[4];
+            char_array[0] = 'T';
+            char_array[1] = 'f';
+            char_array[2] = 'a';
+            char_array[3] = 's';
+
+            Console.WriteLine("Unsorted int array");
+
+            foreach (var item in int_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted int array");
+
+            Sorting(int_array, CompareInt);
+
+            foreach (var item in int_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Unsorted double array");
+
+            foreach (var item in double_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted double array");
+
+            Sorting(double_array, CompareDouble);
+
+            foreach (var item in double_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Unsorted char array");
+
+            foreach (var item in char_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Sorted char array");
+
+            Sorting(char_array, CompareChar);
+
+            foreach (var item in char_array)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static int CompareInt(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else
+            {
+                if (a == b)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+
+        public static int CompareDouble(double a, double b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else
+            {
+                if (a == b)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+
+        public static int CompareChar(char a, char b)
+        {
+            if ((int)char.ToLower(a) > (int)char.ToLower(b))
+            {
+                return 1;
+            }
+            else
+            {
+                if ((int)char.ToLower(a) == (int)char.ToLower(b))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
