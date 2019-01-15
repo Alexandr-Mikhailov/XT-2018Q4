cd0d3eba2b9a7ebb829321a1fcb200e3c04c9513
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2019-01-16 00:57:31 +0400

    Task 07

---
+++ Epam.Task07/Epam.Task07.Number validator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Epam.Task07.Number_validator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            string input;
+            string regNormalNotation = @"^(-?[0-9]+(\.[0-9]+)?)$";
+            string regScienceNotation = @"^(-?[0-9]{1}(\.[0-9]+)?[Ee]-?[0-9]+)$";
+
+            do
+            {
+                Console.WriteLine("Input number to analyze or q to exit");
+                input = Console.ReadLine();
+
+                if (input == "q" || input == "Q")
+                {
+                    break;
+                }
+
+                var regexNormal = new Regex(regNormalNotation);
+                var regexScience = new Regex(regScienceNotation);
+
+                var matchNormal = regexNormal.Match(input);
+                var matchScience = regexScience.Match(input);
+
+                if (matchNormal.Success)
+                {
+                    Console.WriteLine($"The number \"{input}\" is in normal notation");
+                }
+                else
+                {
+                    if (matchScience.Success)
+                    {
+                        Console.WriteLine($"The number \"{input}\" is in science notation");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The \"{input}\" is not a number");
+                    }
+                }
+            }
+            while (true);
+        }
+    }
+}
