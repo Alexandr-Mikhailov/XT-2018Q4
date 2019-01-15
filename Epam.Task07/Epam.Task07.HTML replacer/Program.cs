f00f5b9e10164e76f2690d8567a7c976fa4257a5
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2019-01-15 23:58:13 +0400

    Task 07

---
+++ Epam.Task07/Epam.Task07.HTML replacer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Epam.Task07.HTML_replacer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            string input;
+            string regString = @"<.*?>";
+
+            do
+            {
+                Console.WriteLine("Input text to analyze or q to exit");
+                input = Console.ReadLine();
+                input = @"<b>Это</b> текст <i>с</i> <font >HTML</font> кодами";
+                if (input == "q" || input == "Q")
+                {
+                    break;
+                }
+
+                var regex = new Regex(regString);
+
+                var match = regex.Matches(input);
+
+                //if (match.Success)
+                //{
+                //    Console.WriteLine($"The text \"{input}\" contains date");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"The text \"{input}\" is not contains date");
+                //}
+            }
+            while (true);
+        }
+    }
+}
---
+++ Epam.Task07/Epam.Task07.Time counter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Epam.Task07.Time_counter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public static void Run()
+        {
+            string input;
+            string regString = @"\b(([0-1]?[0-9]|[2][0-3]):([0-5][0-9]))\b";
+
+            do
+            {
+                Console.WriteLine("Input text to analyze or q to exit");
+                input = Console.ReadLine();
+
+                if (input == "q" || input == "Q")
+                {
+                    break;
+                }
+
+                var regex = new Regex(regString);
+
+                var match = regex.Matches(input);
+
+                Console.WriteLine($"The text contains dates {match.Count} time(s)");
+            }
+            while (true);
+        }
+    }
+}
